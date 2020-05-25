060ef1de75baacbbf620c3f312d7495292f82a19
Matthew Beardsley <matthewbeardsley@Network.local>
2020-05-25 15:32:58 +0800

    removed london2012 image, site simpler

---
+++ public/index.js
@@ -0,0 +1,22 @@
+colors = [
+  "firebrick",
+  "seagreen",
+  "lightcoral",
+  "darkolivegreen",
+  "lightseagreen",
+  "lightskyblue",
+  "thistle",
+  "orange",
+  "darkkhaki",
+];
+
+document.body.onload = function () {
+  document.body.style.background =
+    colors[Math.floor(Math.random() * colors.length)];
+};
+
+function changeColour(value) {
+  console.log("hello", value);
+  document.body.style.backgroundColor = value;
+  // document.querySelector('body').style.color(value)
+}
