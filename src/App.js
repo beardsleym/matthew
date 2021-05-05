c5dc826c6e83dfd34d7482d65eb78c9343d482b7
Matthew Beardsley <matthew@beardsley.com.au>
2021-05-05 17:43:10 +0800

    tailwind installed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
+import Card from './components/card';
 
 function App() {
   return (
     <div className="App">
-      
+      <Card />
       <div className="center-me">
 				<img
-					className="portrait"
+					className="w-32 h-32 md:w-48 rounded-full mx-auto"
 					sizes="(max-width: 1400px) 100vw, 1400px"
 					srcSet="./images/matt_vcd2hp_c_scale,w_1052.jpg 1052w"
 					src="./images/matt_vcd2hp_c_scale,w_1400.jpg"
 					alt=""></img>
+          <h1 className="text-lg font-semibold">Test</h1>
 					<h2>
 						<a rel="me" href="http://au.linkedin.com/in/matthewbeardsley" title="LinkedIn">
 							<span className="fab fa-linkedin"></span>
