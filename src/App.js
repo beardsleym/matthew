e134fd990757a2d2ae072e177a00b7b84395a397
Matthew Beardsley <matthew@beardsley.com.au>
2021-05-06 11:10:08 +0800

    project profile basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import Card from './components/card';
+import Card from './components/Card';
+import Navbar from './components/Navbar';
 
 function App() {
+  const projects = [
+    {title: 'RentReef', link: "https://rentreef.com", tags: ["angular","bootstrap"], img: "images/rentreef.png", text:"Peep to peer rental marketplace to rent almost anything"},
+    {title: 'Showcase Base', link: "https://showcasebase.com", tags: ["nuxt","vuetify"], img: "images/showcasebase.png", text:"Showcase Base connects drama school graduates and the people who want to sign them, cast them, and work with them. All in one place, 24/7."},
+    {title: 'Tinito.App', link: "https://tinito.app", tags: ["nuxt","vuetify"], img: "images/tinitoapp.png", text:"URL Link Shortener with dashboard, custom links and statistics"},
+    {title: 'Halal Investors', link: "https://halalinvestors.com", tags: ["vue","tailwindCSS"], img: "images/halalinvestors.png", text:"In addition to examining a company's business and finances, we look at its environmental and social impact as well as its track record for governance (ESG). Our Halal Report Cards give you a quick summary of our comfort level investing in a company."},
+    {title: 'Beardsley.com.au', link: "https://beardsley.com.au", tags: ["react","tailwindCSS"], img: "images/profilesite.png", text:"Profile site, showcasing a few active projects over the past 12 months"}
+  ]
   return (
     <div className="App">
-      <Card />
+      <Navbar />
+        <div class="p-2 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {projects.map((project) => (
+            <Card project={project}/>
+          ))}
+        </div>
       <div className="center-me">
 				<img
 					className="w-32 h-32 md:w-48 rounded-full mx-auto"
@@ -24,28 +37,28 @@ function App() {
   );
 }
 
-const colors = [
-  "firebrick",
-  "seagreen",
-  "lightcoral",
-  "darkolivegreen",
-  "lightseagreen",
-  "lightskyblue",
-  "thistle",
-  "orange",
-  "darkkhaki",
-];
+// const colors = [
+//   "firebrick",
+//   "seagreen",
+//   "lightcoral",
+//   "darkolivegreen",
+//   "lightseagreen",
+//   "lightskyblue",
+//   "thistle",
+//   "orange",
+//   "darkkhaki",
+// ];
 
-function changeColour() {
-  document.body.style.background =
-    colors[Math.floor(Math.random() * colors.length)];
-}
+// function changeColour() {
+//   document.body.style.background =
+//     colors[Math.floor(Math.random() * colors.length)];
+// }
 
-document.body.onload = function () {
-  changeColour()
-  setInterval(()=> {
-    changeColour()
-  },5000)
-};
+// document.body.onload = function () {
+//   changeColour()
+//   setInterval(()=> {
+//     changeColour()
+//   },5000)
+// };
 
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -1,22 +1,26 @@
-const card = () => {
+const Card = (props) => {
+  const {title, text, tags, img, link} = props.project
   return ( 
     <div className="p-10">  
+    <a href={link} target="_blank" rel="noreferrer">
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src="/mountain.jpg" alt="Mountain"></img>
+        <img className="w-full" src={img} alt="RentReef"></img>  
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Mountain</div>
+        <div className="font-bold text-xl mb-2">{title}</div>
         <p className="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          {text}
         </p>
       </div>
       <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        {tags.map((tag) => (
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{tag}</span>
+          )
+          )}
       </div>
     </div>
+  </a>
   </div>
    );
 }
  
-export default card;
+export default Card;
