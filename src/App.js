96f5903cf9d9423099315d9591014390323292b3
Matthew Beardsley <matthew@beardsley.com.au>
2021-05-07 15:12:04 +0800

    add empty footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Card from './components/Card';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
 function App() {
   const projects = [
-    {title: 'RentReef', link: "https://rentreef.com", tags: ["angular","bootstrap"], img: "images/rentreef.png", text:"Peep to peer rental marketplace to rent almost anything"},
-    {title: 'Showcase Base', link: "https://showcasebase.com", tags: ["nuxt","vuetify"], img: "images/showcasebase.png", text:"Showcase Base connects drama school graduates and the people who want to sign them, cast them, and work with them. All in one place, 24/7."},
-    {title: 'Tinito.App', link: "https://tinito.app", tags: ["nuxt","vuetify"], img: "images/tinitoapp.png", text:"URL Link Shortener with dashboard, custom links and statistics"},
-    {title: 'Halal Investors', link: "https://halalinvestors.com", tags: ["vue","tailwindCSS"], img: "images/halalinvestors.png", text:"In addition to examining a company's business and finances, we look at its environmental and social impact as well as its track record for governance (ESG). Our Halal Report Cards give you a quick summary of our comfort level investing in a company."},
-    {title: 'Beardsley.com.au', link: "https://beardsley.com.au", tags: ["react","tailwindCSS"], img: "images/profilesite.png", text:"Profile site, showcasing a few active projects over the past 12 months"}
+    {title: 'RentReef', link: "https://rentreef.com", tags: ["angular","bootstrap", "product"], img: "images/rentreef.png", text:"Peep to peer rental marketplace to rent almost anything"},
+    {title: 'Showcase Base', link: "https://showcasebase.com", tags: ["nuxt","vuetify", "full-stack"], img: "images/showcasebase.png", text:"Showcase Base connects drama school graduates and the people who want to sign them, cast them, and work with them. All in one place, 24/7."},
+    {title: 'Tinito.App', link: "https://tinito.app", tags: ["nuxt","vuetify", "full-stack"], img: "images/tinitoapp.png", text:"URL Link Shortener with dashboard, custom links and statistics"},
+    {title: 'Halal Investors', link: "https://halalinvestors.com", tags: ["vue","tailwindCSS", "full-stack"], img: "images/halalinvestors.png", text:"In addition to examining a company's business and finances, we look at its environmental and social impact as well as its track record for governance (ESG). Our Halal Report Cards give you a quick summary of our comfort level investing in a company."},
+    {title: 'Beardsley.com.au', link: "https://beardsley.com.au", tags: ["react","tailwindCSS", "full-stack"], img: "images/profilesite.png", text:"Profile site, showcasing a few active projects over the past 12 months"}
   ]
   return (
     <div className="App">
@@ -17,13 +18,12 @@ function App() {
       <div class="max-w-screen-xl mx-auto px-4">
         {/* Grid wrapper */}
         <div class="-mx-4 flex flex-wrap">
-          {/* <div class="p-2 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"> */}
             {projects.map((project) => (
               <Card project={project}/>
             ))}
-          {/* </div> */}
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -14,7 +14,7 @@ const Card = (props) => {
       </div>
       <div className="px-6 pt-4 pb-2">
         {tags.map((tag) => (
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{tag}</span>
+          <span className="inline-block bg-gray-200 hover:bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{tag}</span>
           )
           )}
       </div>
