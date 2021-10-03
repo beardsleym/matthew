c9cad1945fe19c3d6a56ff30c211fbf7e4b04421
Matthew Beardsley <matthew@beardsley.com.au>
2021-10-03 21:17:52 +0800

    hide mobile menu icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,14 +24,14 @@ export default function Example(props) {
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                {/* <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
                     <MenuIcon className="block h-6 w-6" aria-hidden="true" />
                   )}
-                </Disclosure.Button>
+                </Disclosure.Button> */}
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
@@ -86,7 +86,7 @@ export default function Example(props) {
                 {/* Profile dropdown */}
                 <a href="https://linkedin.com/in/matthewbeardsley">
                   <img
-                    className="w-6 mr-2"
+                    className="w-6 mr-4"
                     src="https://cdn.worldvectorlogo.com/logos/linkedin-icon.svg"
                     alt=""
                   />
